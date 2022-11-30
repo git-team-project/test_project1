a7dcb75e43f5f13c079f90f7c68f6e9d8ea7c0f0
YSU\tsd03 <tsd03150@naver.com>
2022-11-30 09:07:21 +0900

    [feat] 로그인 기능 추가

--- Mongo_Jdbc/src/main/java/people/PeopleDao.java
+++ Mongo_Jdbc/src/main/java/people/PeopleDao.java
@@ -3,6 +3,9 @@ package people;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Sorts.ascending;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +13,12 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
@@ -23,6 +28,38 @@ import com.mongodb.client.result.UpdateResult;
 import util.MongoInfo;
 
 public class PeopleDao {
+	
+	static String entyptPassword(String id,String pw) throws NoSuchAlgorithmException{
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+		// 평문+id 암호화
+		md.update(id.getBytes());
+		pw = String.format("%064x", new BigInteger(1, md.digest()));
+		System.out.println("raw+salt의 해시값 : "+ pw);
+		return pw;
+	}
+	
+	public boolean loginCheck(String memberId, String pw) throws NoSuchAlgorithmException{
+		try(MongoClient mongoClient = MongoClients.create(MongoInfo.getUri())) {
+			MongoDatabase database = mongoClient.getDatabase(MongoInfo.getDataBase());
+			MongoCollection<Document> collection = database.getCollection("member");
+			
+			// db에 암호화된 비번을 select하기 위해
+			// 입력한 비번도 암호화
+			pw = entyptPassword(memberId, pw);
+			
+			Bson query = Filters.and(
+                    Filters.eq("memberId", memberId), 
+                    Filters.eq("pw", pw));
+			
+			Document doc = collection.find(query).first();			
+			if(doc != null) {
+				return true;
+			} else {
+				return false;
+			}
+		} 
+	}
 	
 	public List<People> findAll() {
 		List<Document> findList = new ArrayList<>();
@@ -106,4 +143,5 @@ public class PeopleDao {
 			return result.wasAcknowledged();
 		}
 	}
+	
 }
